18a52942c60063b78e033446642ef4e2826d00c4
Ben James Dodwell <61091068+ben-james-dodwell@users.noreply.github.com>
2024-05-01 15:19:04 +0100

    Initial commit
    Deploy Terraform backend for S3 state storage and DynamoDB locking

---
+++ main.tf
@@ -0,0 +1,46 @@
+resource "aws_s3_bucket" "terraform_bucket" {
+  bucket = "cv-benjamesdodwell-com-terraform"
+}
+
+resource "aws_s3_bucket_versioning" "terraform_versioning" {
+  bucket = aws_s3_bucket.terraform_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_object_lock_configuration" "terraform_object_lock" {
+  bucket = aws_s3_bucket.terraform_bucket.id
+
+  rule {
+    default_retention {
+      mode = "COMPLIANCE"
+      days = 5
+    }
+  }
+
+  depends_on = [
+    aws_s3_bucket_versioning.terraform_versioning
+  ]
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_encryption" {
+  bucket = aws_s3_bucket.terraform_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_table" {
+  name           = "cv-benjamesdodwell-com-terraform"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
